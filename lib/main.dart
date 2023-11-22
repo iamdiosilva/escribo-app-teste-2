cc8e7282352e793411068f122cb893cfd8bf30f0
Dio <diosaves@gmail.com>
2023-11-22 11:47:49 -0300

    fix: app rotatation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/modules/app_module.dart';
@@ -7,10 +6,5 @@ import 'app/modules/app_widget.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
